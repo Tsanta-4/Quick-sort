d8f4e01bb2d24be8bc28d423a5b45dad7439fd54
Tsanta-4 <tsanta8nirina@gmail.com>
2020-12-08 23:26:34 +0100

    add modification

--- Utils.py
+++ Utils.py
@@ -1,16 +1,17 @@
 def quick_sort(tab, start, last):
 	"""Fonction tri rapide"""
 	if start <= last:
-		pivot = partition(tab, start, last)
+		pivot = last
+		pivot = partition(tab, start, last, pivot)
 		quick_sort(tab, start, pivot-1)
 		quick_sort(tab, pivot+1, last)
 
-def partition(tab, start, last):
+def partition(tab, start, last, pivot):
 	"""fonction qui permet de partitionner les sous-tableau"""
+	tab[pivot], tab[last] = tab[last], tab[pivot]
 	j = start
-	pivot = tab[last]
 	for i in range(start, last):
-		if tab[i] <= pivot:
+		if tab[i] <= tab[last]:
 			tab[i], tab[j] = tab[j], tab[i]
 			j += 1
 	tab[last], tab[j] = tab[j], tab[last]
