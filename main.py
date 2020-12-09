42baf66d1a06492fc9f9270debfdda6f97149132
Tsanta-4 <tsanta8nirina@gmail.com>
2020-12-09 17:50:01 +0100

    --

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 #~ l = [8, 6, 1, 4, 2, 9, 10]
 #~ l = [3,7,8,5,2,1,9,5,4]
 #~ l = [x for x in range(100000,-1,-1)]
-l = [5]*1000
+l = [1]*100 + [2, 3, 43, 5, 7]
 n = len(l)
 start = time.time()
 u.quick_sort(l,0,n-1)
