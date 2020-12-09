6f82e32b344a0a2a73668752071ddd1ad05e2f10
Tsanta-4 <tsanta8nirina@gmail.com>
2020-12-09 17:44:26 +0100

    --

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from os import system
 import Utils as u
+import time 
 
 #~ l = [8, 6, 1, 4, 2, 9, 10]
-l = [3,7,8,5,2,1,9,5,4]
+#~ l = [3,7,8,5,2,1,9,5,4]
+#~ l = [x for x in range(100000,-1,-1)]
+l = [5]*1000
 n = len(l)
+start = time.time()
 u.quick_sort(l,0,n-1)
-print(l)
+end = time.time()
+#~ print(l)
+print("Temps d'execution = ", end-start)
 
 
 system("Pause")
